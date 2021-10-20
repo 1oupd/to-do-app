3e22613ddf3d5111f9339d5b6077ab72bf8c9a5a
danny vincent <louisa.duncan44@outlook.com>
2021-10-20 22:42:41 +0100

    Added icons from react-icons

--- src/components/List.js
+++ src/components/List.js
@@ -1,25 +1,36 @@
-import React, {useState} from 'react'
-import Form from './Form'
+import React, { useState } from "react";
+import Form from "./Form";
+import ToDo from "./ToDo";
 
 function List() {
-    const [items, setItems] = useState([]);
+  const [items, setItems] = useState([]);
 
-    const addItem = item => {
-        if(!item.text || /^\s*$/.test(item.text)){
-            return
-        }
-
-        const newItems = [item, ...items]
-
-        setItems(newItems)
+  const addItem = (item) => {
+    if (!item.text || /^\s*$/.test(item.text)) {
+      return;
     }
 
-    return (
-        <div>
-            <h1>What would you like to do?</h1>        
-            <Form onSubmit={addItem} />
-        </div>
-    )
+    const newItems = [item, ...items];
+
+    setItems(newItems);
+  };
+
+  const completeItem = (id) => {
+    let updatedItems = items.map((item) => {
+      if (item.id === id) {
+        item.isComplete = !item.isComplete;
+      }
+      return item;
+    });
+    setItems(updatedItems);
+  };
+  return (
+    <div>
+      <h1>What do you have in mind?</h1>
+      <Form onSubmit={addItem} />
+      <ToDo items={items} completeItem={completeItem} />
+    </div>
+  );
 }
 
-export default List
+export default List;
--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,13 +1,31 @@
+import React, { useState } from "react";
+// import Form from "./Form";
+import { FiEdit3 } from "react-icons/fi";
+import { RiCloseCircleLine } from "react-icons/ri";
 
-import React, { useState } from "react"
-import Form from "./Form"
+function ToDo({items, completeItem}) {
+  const [edit, setEdit] = useState({
+    id: null,
+    value: ""
+  })
 
-function ToDo() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return items.map((item, index) => (
+    <div
+      className={item.isComplete ? "item-row complete" : "item-row"}
+      key={index}
+    >
+      <div key={item.id} onClick={() => completeItem(item.id)}>
+        {item.text}
+      </div>
+      <div className="icons">
+        <RiCloseCircleLine 
+        className="delete-icon"
+        onClick={ () => removeItem(item.id)}
+            />
+        <FiEdit3 />
+      </div>
+    </div>
+  ));
 }
 
-export default ToDo
+export default ToDo;
